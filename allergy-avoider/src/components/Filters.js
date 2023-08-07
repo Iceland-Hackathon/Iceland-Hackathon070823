fce5cfe965591e8eae599c3c2e644af117eebacf
anjaliruth <ruthanjali0211@gmail.com>
2023-08-07 03:17:09 -0700

    added dropdown

--- allergy-avoider/src/components/Filters.js
+++ allergy-avoider/src/components/Filters.js
@@ -1,7 +1,40 @@
-function Filters() {
-    return (
-      <p>Filters</p>
-    );
-  }
-  
-  export default Filters;
+
+
+function Filters({diet, allergies, setDiet, setAllergies}) {
+
+
+
+function handleDietChange(e) {
+  setDiet(e.target.value)
+}
+
+
+
+console.log(diet)
+  return (
+    <>
+      {/* dietary requirements dropown */}
+      <select onChange={handleDietChange}>
+        <option value="">Dietary Preferences</option>
+        <option value="Halal">Halal</option>
+        <option value="Kosher">Kosher</option>
+        <option value="Vegan">Vegan</option>
+        <option value="Vegetarian">Vegetarian</option>
+        <option value="Pescetarian">Pescetarian</option>
+      </select>
+      {/* allergies checkbox */}
+      <label >
+      <input type="checkbox" value="Peanuts" /> Peanuts
+      </label>
+      <label >
+      <input type="checkbox" value="Eggs" /> Eggs
+      </label>
+      <label >
+      <input type="checkbox" value="Dairy" /> Dairy
+      </label>
+    
+    </>
+  );
+}
+
+export default Filters;
