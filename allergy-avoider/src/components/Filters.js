14dd45b6cc3fff6f8dadccdb8b609bb0e51bec23
paulg44 <paulgarton1984@hotmail.com>
2023-08-07 13:36:04 +0100

    start styling

--- allergy-avoider/src/components/Filters.js
+++ allergy-avoider/src/components/Filters.js
@@ -1,18 +1,13 @@
+import "./Filter.css";
 
+function Filters({ diet, allergies, setDiet, setAllergies }) {
+  function handleDietChange(e) {
+    setDiet(e.target.value);
+  }
 
-function Filters({diet, allergies, setDiet, setAllergies}) {
-
-
-
-function handleDietChange(e) {
-  setDiet(e.target.value)
-}
-
-
-
-console.log(diet)
+  console.log(diet);
   return (
-    <>
+    <div className="filters">
       {/* dietary requirements dropown */}
       <select onChange={handleDietChange}>
         <option value="">Dietary Preferences</option>
@@ -23,17 +18,21 @@ console.log(diet)
         <option value="Pescetarian">Pescetarian</option>
       </select>
       {/* allergies checkbox */}
-      <label >
-      <input type="checkbox" value="Peanuts" /> Peanuts
-      </label>
-      <label >
-      <input type="checkbox" value="Eggs" /> Eggs
-      </label>
-      <label >
-      <input type="checkbox" value="Dairy" /> Dairy
-      </label>
-    
-    </>
+      <div className="allergies">
+        <h3>Allergies</h3>
+        <div className="allergyLabels">
+          <label>
+            <input type="checkbox" value="Peanuts" /> Peanuts
+          </label>
+          <label>
+            <input type="checkbox" value="Eggs" /> Eggs
+          </label>
+          <label>
+            <input type="checkbox" value="Dairy" /> Dairy
+          </label>
+        </div>
+      </div>
+    </div>
   );
 }
 
--- allergy-avoider/src/components/Header.js
+++ allergy-avoider/src/components/Header.js
@@ -1,7 +1,12 @@
+import "./Header.css";
+
 function Header() {
-    return (
-      <p>Header</p>
-    );
-  }
-  
-  export default Header;
+  return (
+    <div className="header">
+      <h1>Safe Plate</h1>
+      <img src="https://images.unsplash.com/photo-1633018953392-c0f59c5447e6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aWNlbGFuZCUyMGZsYWd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"></img>
+    </div>
+  );
+}
+
+export default Header;
