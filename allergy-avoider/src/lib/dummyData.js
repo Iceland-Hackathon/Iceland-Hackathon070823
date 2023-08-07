96f21bb2c1d587d8841dc6fe71398524580d5af7
CGreen789 <112316339+CGreen789@users.noreply.github.com>
2023-08-07 14:08:33 +0100

    Filtering working on both diet and allergies
    Co-authored-by: Deen-q <Deen-q@users.noreply.github.com>
    Co-authored-by: Dave <davidhuckfield@users.noreply.github.com>

--- allergy-avoider/src/lib/dummyData.js
+++ allergy-avoider/src/lib/dummyData.js
@@ -1,52 +1,52 @@
 const productsData = [
   {
-      id: 1,
-      name: "Vegan Salad",
-      dietaryPreference: "Vegan",
-      allergens: []
+    id: 1,
+    name: "Vegan Salad",
+    dietaryPreference: "Vegan",
+    allergens: [],
   },
   {
-      id: 2,
-      name: "Vegetarian Pasta",
-      dietaryPreference: "Vegetarian",
-      allergens: ["Dairy"]
+    id: 2,
+    name: "Vegetarian Pasta",
+    dietaryPreference: "Vegetarian",
+    allergens: ["Dairy", "Peanuts"],
   },
   {
-      id: 3,
-      name: "Pescetarian Sushi",
-      dietaryPreference: "Pescetarian",
-      allergens: []
+    id: 3,
+    name: "Pescetarian Sushi",
+    dietaryPreference: "Pescetarian",
+    allergens: [],
   },
   {
-      id: 4,
-      name: "Plain Rice",
-      dietaryPreference: "None",
-      allergens: []
+    id: 4,
+    name: "Plain Rice",
+    dietaryPreference: "None",
+    allergens: [],
   },
   {
-      id: 5,
-      name: "Chicken Sandwich",
-      dietaryPreference: "None",
-      allergens: []
+    id: 5,
+    name: "Chicken Sandwich",
+    dietaryPreference: "None",
+    allergens: [],
   },
   {
-      id: 6,
-      name: "Cheese Pizza",
-      dietaryPreference: "None",
-      allergens: ["Dairy"]
+    id: 6,
+    name: "Cheese Pizza",
+    dietaryPreference: "None",
+    allergens: ["Dairy"],
   },
   {
-      id: 7,
-      name: "Peanut Butter Cookies",
-      dietaryPreference: "None",
-      allergens: ["Peanuts"]
+    id: 7,
+    name: "Peanut Butter Cookies",
+    dietaryPreference: "None",
+    allergens: ["Peanuts", "Eggs"],
   },
   {
-      id: 8,
-      name: "Shrimp Pasta",
-      dietaryPreference: "None",
-      allergens: ["Crustaceans"]
-  }
+    id: 8,
+    name: "Shrimp Pasta",
+    dietaryPreference: "None",
+    allergens: ["Crustaceans"],
+  },
 ];
 
 export default productsData;
