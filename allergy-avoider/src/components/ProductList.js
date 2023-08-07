7d2252732c004b119eff5101e32a79ec41a39c83
CGreen789 <112316339+CGreen789@users.noreply.github.com>
2023-08-07 12:02:14 +0100

    filtering through the dummy data for dietary pref

--- allergy-avoider/src/components/ProductList.js
+++ allergy-avoider/src/components/ProductList.js
@@ -1,15 +1,41 @@
 import ProductCard from "./ProductCard";
 import dummyData from "../lib/dummyData.js";
+import { useState } from "react";
 
-function ProductList() {
+// Function that filters the array and renders only the products that meet that criteria
+// useState to save the renderedList
+// checks the array (dietaryPreference) if same as diet, then show it
+// Update setProductList depending on the filter
+
+// filter through dummydata
+// Set a new variable newData
+// Set productsList to newData
+
+function ProductList({ allergies, diet }) {
+  // const [productList, setProductList] = useState(dummyData);
+
+  const filteredArray = dummyData.filter((item) => {
+    let dietaryPreference = diet === "" || item.dietaryPreference === diet;
+    return dietaryPreference;
+  });
+
+  // function filterDietryRequirements (dietaryPreference) {
+  //   if (dietaryPreference === diet)
+  //   setProductList(filteredArray)
+
+  // // const x = dummyData.filter(() =>
+  // //
+  // //);
+  // }
+
   return (
     <div>
       {/* Map through each item in list that matches filters */}
       <ul>
-        {dummyData.map((food) => (
-            <li>
-              <ProductCard key={food.id} name={food.name} />{" "}
-            </li>
+        {filteredArray.map((food) => (
+          <li>
+            <ProductCard key={food.id} name={food.name} />{" "}
+          </li>
         ))}
       </ul>
     </div>
