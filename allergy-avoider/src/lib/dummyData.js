d6f197f871330bf79bbbb91ac3b1735a0248c837
CGreen789 <112316339+CGreen789@users.noreply.github.com>
2023-08-07 11:19:42 +0100

    Added ids to dummydata and productList showing
    Co-authored-by: Deen-q <Deen-q@users.noreply.github.com>

--- allergy-avoider/src/lib/dummyData.js
+++ allergy-avoider/src/lib/dummyData.js
@@ -1,47 +1,52 @@
-// productsData.js
-
 const productsData = [
-    {
+  {
+      id: 1,
       name: "Vegan Salad",
       dietaryPreference: "Vegan",
       allergens: []
-    },
-    {
+  },
+  {
+      id: 2,
       name: "Vegetarian Pasta",
       dietaryPreference: "Vegetarian",
       allergens: ["Dairy"]
-    },
-    {
+  },
+  {
+      id: 3,
       name: "Pescetarian Sushi",
       dietaryPreference: "Pescetarian",
       allergens: []
-    },
-    {
+  },
+  {
+      id: 4,
       name: "Plain Rice",
       dietaryPreference: "None",
       allergens: []
-    },
-    {
+  },
+  {
+      id: 5,
       name: "Chicken Sandwich",
       dietaryPreference: "None",
       allergens: []
-    },
-    {
+  },
+  {
+      id: 6,
       name: "Cheese Pizza",
       dietaryPreference: "None",
       allergens: ["Dairy"]
-    },
-    {
+  },
+  {
+      id: 7,
       name: "Peanut Butter Cookies",
       dietaryPreference: "None",
       allergens: ["Peanuts"]
-    },
-    {
+  },
+  {
+      id: 8,
       name: "Shrimp Pasta",
       dietaryPreference: "None",
       allergens: ["Crustaceans"]
-    }
-  ];
-  
-  export default productsData;
-  
+  }
+];
+
+export default productsData;
