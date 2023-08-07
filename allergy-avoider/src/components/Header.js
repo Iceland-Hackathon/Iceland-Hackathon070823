7cdc60d08edc9a31bb5ae5338e69728dbc7e83a3
paulg44 <paulgarton1984@hotmail.com>
2023-08-07 14:32:55 +0100

    deployment testing

--- allergy-avoider/src/components/Header.js
+++ allergy-avoider/src/components/Header.js
@@ -4,7 +4,10 @@ function Header() {
   return (
     <div className="header">
       <h1>Safe Plate</h1>
-      <img src="https://images.unsplash.com/photo-1633018953392-c0f59c5447e6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aWNlbGFuZCUyMGZsYWd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"></img>
+      <img
+        src="https://images.unsplash.com/photo-1633018953392-c0f59c5447e6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aWNlbGFuZCUyMGZsYWd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
+        alt="main"
+      ></img>
     </div>
   );
 }
