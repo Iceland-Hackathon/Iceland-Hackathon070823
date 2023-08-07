2c877873e5c3a95bfb1c8d83317df511f16a7397
anjaliruth <ruthanjali0211@gmail.com>
2023-08-07 02:44:44 -0700

    added dummyData

---
+++ allergy-avoider/src/lib/dummyData.js
@@ -0,0 +1,47 @@
+// productsData.js
+
+const productsData = [
+    {
+      name: "Vegan Salad",
+      dietaryPreference: "Vegan",
+      allergens: []
+    },
+    {
+      name: "Vegetarian Pasta",
+      dietaryPreference: "Vegetarian",
+      allergens: ["Dairy"]
+    },
+    {
+      name: "Pescetarian Sushi",
+      dietaryPreference: "Pescetarian",
+      allergens: []
+    },
+    {
+      name: "Plain Rice",
+      dietaryPreference: "None",
+      allergens: []
+    },
+    {
+      name: "Chicken Sandwich",
+      dietaryPreference: "None",
+      allergens: []
+    },
+    {
+      name: "Cheese Pizza",
+      dietaryPreference: "None",
+      allergens: ["Dairy"]
+    },
+    {
+      name: "Peanut Butter Cookies",
+      dietaryPreference: "None",
+      allergens: ["Peanuts"]
+    },
+    {
+      name: "Shrimp Pasta",
+      dietaryPreference: "None",
+      allergens: ["Crustaceans"]
+    }
+  ];
+  
+  export default productsData;
+  
